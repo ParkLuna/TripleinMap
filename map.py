09829ae02735ac852ac165cdf76ffe471d16af9c
ParkLuna <141165285+ParkLuna@users.noreply.github.com>
2023-09-09 01:55:23 +0530

    map.py

--- map.py
+++ map.py
@@ -1,6 +1,14 @@
+try:
+    input_numbers = input("Enter a list of integers separated by spaces: ").split()
+    input_numbers = list(map(int, input_numbers))
+except ValueError:
+    print("Invalid input. Please enter a valid list of integers separated by spaces.")
+    exit()
+
 def triple_number(num):
     return num * 3
-sample_list = [1, 2, 3, 4, 5, 6, 7]
-result_list = list(map(triple_number, sample_list))
-print("Triple of list numbers:")
-print(result_list)
+
+tripled_numbers = list(map(triple_number, input_numbers))
+
+print("Original List:", input_numbers)
+print("Tripled List:", tripled_numbers)
